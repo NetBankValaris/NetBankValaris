9569c8f449f545e7d1981c8e1c9078d6d19b3e3c
NetBankValaris <netbankvalaris@gmail.com>
2022-11-23 01:31:09 -0600

    21/11/22

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from select import select
 from flask import Flask, session, render_template, request, redirect, url_for
 #from flaskext.mysql import MySQL
 import pymysql
@@ -7,7 +6,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-
+NU = ""
 UPPY = "true"
 
 @app.route('/')
@@ -16,6 +15,8 @@ def index():
 
 @app.route("/ac_reg", methods=["POST"])
 def ac_reg():
+    global NU
+    global UPPY
     if request.method=="POST":
         NomProy = request.form["NomProy"]
         intdat = request.form["intdat"]
@@ -24,15 +25,17 @@ def ac_reg():
         img = request.form["Arch"]
         fecha = datetime.today().strftime('%Y-%m-%d')
 
-        conn = pymysql.connect(host="localhost", user="root", passwd="", db="proyectos")
+        conn = pymysql.connect(host="NetBankValaris.mysql.pythonanywhere-services.com", user="NetBankValaris", passwd="Valaris102323", db="NetBankValaris$default")
         cursor = conn.cursor()
-        cursor.execute("insert into registro (NomProy, intdat, Nom, Descripcion, Arch, fecha) values(%s, %s, %s, %s, %s, %s)", (NomProy, intdat, Nom, descripcion, img, fecha))
+        cursor.execute("insert into registro (NomProy, intdat, Nom, Descripcion, Arch, fecha, usuario_ref) values(%s, %s, %s, %s, %s, %s, %s)", (NomProy, intdat, Nom, descripcion, img, fecha, NU))
         conn.commit()
+    NU = ""
+    UPPY = "true"
     return redirect(url_for("index"))
     
 @app.route('/validados', methods=["GET"])
 def validados():
-    conn = pymysql.connect(host='localhost', user='root', passwd='', db='proyectos' )
+    conn = pymysql.connect(host="NetBankValaris.mysql.pythonanywhere-services.com", user="NetBankValaris", passwd="Valaris102323", db="NetBankValaris$default")
     cursor = conn.cursor()
     cursor.execute('select NomProy, descripcion from registro where verificado = 1')
     datos = cursor.fetchall()
@@ -46,20 +49,35 @@ def registro():
 
 @app.route("/verifi", methods = ["POST"])
 def Verifi():
+    global NU
     global UPPY
+    UPPY = "true"
     if request.method == "POST":
         Usuario = request.form["Usu"]
         Contra = request.form["Cont"]
 
-        conn = pymysql.connect(host="localhost", user="root", passwd="", db="prot")
+        #conn = pymysql.connect(host="localhost", user="root", passwd="", db="prot")
+        conn = pymysql.connect(host="NetBankValaris.mysql.pythonanywhere-services.com", user="NetBankValaris", passwd="Valaris102323", db="NetBankValaris$default")
         cursor = conn.cursor()
-        cursor.execute("select * from validados")
+        cursor.execute("select a.usuario, a.contraseña, b.usuario_ref from validados a left join registro b on a.usuario = b.usuario_ref")
         datos = cursor.fetchall()
+        print(datos)
         for i in datos:
-            if Usuario == i[0] and Contra == i[1]:
+            if (Usuario == i[0] and Contra == i[1] and i[2] == None):
+                print("Hola")
                 UPPY = "false"
+                NU = i[0]
 
         return render_template("registro.html" , UP = UPPY)
+
+@app.route("/proyectos")
+def proyectos():
+    conn = pymysql.connect(host="NetBankValaris.mysql.pythonanywhere-services.com", user="NetBankValaris", passwd="Valaris102323", db="NetBankValaris$default")
+    cursor = conn.cursor()
+    cursor.execute("select * from registro where verificado = 1")
+    proys = cursor.fetchall()
+    print(proys)
+    return render_template("proyectos.html", dats = proys)
 
 if __name__ == "__main__":
 
