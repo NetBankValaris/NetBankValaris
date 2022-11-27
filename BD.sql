6dc70d5af0283090f5b54b236fa491d16f7c45c5
NetBankValaris <netbankvalaris@gmail.com>
2022-11-26 19:55:23 -0600

    26/11/2022

--- BD.sql
+++ BD.sql
@@ -11,8 +11,16 @@ usuario varchar(15) not null,
 contraseña varchar(10) not null,
 primary key(usuario) 
 );
+
+drop table if exists categoria;
+create table if not exists categoria(
+idcategoria int(3) auto_increment,
+descrip varchar(45) not null,
+PRIMARY KEY(`idcategoria`)
+);
+
 create table if not exists registro(
-idReg int(11) auto_increment,
+idreg int(11) auto_increment,
 nomproy varchar(100) not null,
 intdat varchar(100) not null,
 nom varchar(100) not null,
@@ -21,7 +29,11 @@ arch blob not null,
 fecha date not null,
 usuario_ref varchar(15) not null,
 verificado int(1),
-primary key(idReg),
-Constraint RegValRel Foreign Key (usuario_ref) references validados(usuario) on delete restrict on update restrict
+categoria_ref int(3),
+primary key(`idreg`),
+Constraint RegValRel Foreign Key (usuario_ref) references validados(usuario) on delete restrict on update restrict,
+Constraint RegCatRel Foreign Key (categoria_ref) references categoria(idcategoria) on delete restrict on update restrict
 );
 insert into validados(usuario, contraseña) values ("Jose Juan", "JoseJuan08"), ("Regina", "Regina09"), ("Elvia Yuridia", "Mimor09");
+
+insert into categoria(descrip) values ("Prototipos"), ("Emprendedor"), ("Robótico"), ("Industrial");
--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, session, render_template, request, redirect, url_for
+from flask import Flask, session, render_template, request, redirect, url_for, flash,send_from_directory
+import os
+from werkzeug.utils import secure_filename
 #from flaskext.mysql import MySQL
 import pymysql
 from datetime import datetime
 
+UPLOAD_FOLDER = '/static/users'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 NU = ""
 UPPY = "true"
@@ -18,35 +23,49 @@ def ac_reg():
     global NU
     global UPPY
     if request.method=="POST":
+        file = request.files["Arch"]
+        basepath = os.path.dirname (__file__)
+        filename = secure_filename(file.filename)
+        extension = os.path.splitext(filename)[1]
+        nuevonombre = NU + extension
+        upload_path = os.path.join(basepath, 'static/users', nuevonombre)
+        file.save(upload_path)
+
+        if "Categ" in request.form:
+            cat = request.form["Categ"]
+        else:
+            cat = 1
         NomProy = request.form["NomProy"]
         intdat = request.form["intdat"]
         Nom = request.form["Nom"]
         descripcion = request.form["descripcion"]
-        img = request.form["Arch"]
         fecha = datetime.today().strftime('%Y-%m-%d')
 
-        conn = pymysql.connect(host="NetBankValaris.mysql.pythonanywhere-services.com", user="NetBankValaris", passwd="Valaris102323", db="NetBankValaris$default")
+        conn = pymysql.connect(host="localhost", user="root", passwd="", db="prot")
         cursor = conn.cursor()
-        cursor.execute("insert into registro (NomProy, intdat, Nom, Descripcion, Arch, fecha, usuario_ref) values(%s, %s, %s, %s, %s, %s, %s)", (NomProy, intdat, Nom, descripcion, img, fecha, NU))
+        cursor.execute("insert into registro (NomProy, intdat, Nom, Descripcion, fecha, usuario_ref, categoria_ref, verificado) values( %s, %s, %s, %s, %s, %s, %s, %s)", (NomProy, intdat, Nom, descripcion, fecha, NU, cat, 1))
         conn.commit()
+
     NU = ""
     UPPY = "true"
     return redirect(url_for("index"))
-    
+#this
 @app.route('/validados', methods=["GET"])
 def validados():
-    conn = pymysql.connect(host="NetBankValaris.mysql.pythonanywhere-services.com", user="NetBankValaris", passwd="Valaris102323", db="NetBankValaris$default")
+    conn = pymysql.connect(host="localhost", user="root", passwd="", db="prot")
     cursor = conn.cursor()
     cursor.execute('select NomProy, descripcion from registro where verificado = 1')
     datos = cursor.fetchall()
-    print(datos)
     return render_template("index.html", req=datos)
-
+#this
 @app.route("/registro")
 def registro():
     global UPPY
+    global NU
+    NU = ""
+    UPPY = "true"
     return render_template("registro.html", UP = UPPY)
-
+#this
 @app.route("/verifi", methods = ["POST"])
 def Verifi():
     global NU
@@ -57,27 +76,45 @@ def Verifi():
         Contra = request.form["Cont"]
 
         #conn = pymysql.connect(host="localhost", user="root", passwd="", db="prot")
-        conn = pymysql.connect(host="NetBankValaris.mysql.pythonanywhere-services.com", user="NetBankValaris", passwd="Valaris102323", db="NetBankValaris$default")
+        conn = pymysql.connect(host="localhost", user="root", passwd="", db="prot")
         cursor = conn.cursor()
         cursor.execute("select a.usuario, a.contraseña, b.usuario_ref from validados a left join registro b on a.usuario = b.usuario_ref")
         datos = cursor.fetchall()
-        print(datos)
         for i in datos:
             if (Usuario == i[0] and Contra == i[1] and i[2] == None):
-                print("Hola")
                 UPPY = "false"
                 NU = i[0]
+        conn = pymysql.connect(host="localhost", user="root", passwd="", db="prot")
+        cursor = conn.cursor()
+        cursor.execute("select * from categoria")
+        datcat = cursor.fetchall()
 
-        return render_template("registro.html" , UP = UPPY)
-
+        return render_template("registro.html" , UP = UPPY, cate = datcat)
+#this
 @app.route("/proyectos")
 def proyectos():
-    conn = pymysql.connect(host="NetBankValaris.mysql.pythonanywhere-services.com", user="NetBankValaris", passwd="Valaris102323", db="NetBankValaris$default")
+    global prim
+    prim = True
+    conn = pymysql.connect(host="localhost", user="root", passwd="", db="prot")
     cursor = conn.cursor()
     cursor.execute("select * from registro where verificado = 1")
     proys = cursor.fetchall()
-    print(proys)
-    return render_template("proyectos.html", dats = proys)
+    cursor.execute("select * from categoria")
+    cat = cursor.fetchall()
+    return render_template("proyectos.html", dats = proys, catTip = cat, Bool = prim,num = 0)
+
+@app.route("/proyectos_fil/<string:id>")
+def proyectos_fil(id):
+    global prim
+    prim = False
+    conn = pymysql.connect(host="localhost", user="root", passwd="", db="prot")
+    cursor = conn.cursor()
+    cursor.execute("select * from registro where verificado = 1 and categoria_ref = %s", (id))
+    proys = cursor.fetchall()
+    cursor.execute("select * from categoria")
+    cat = cursor.fetchall()
+    print(type(id))
+    return render_template("proyectos.html", dats = proys, catTip = cat, num = int(id), Bool = prim)
 
 if __name__ == "__main__":
 
